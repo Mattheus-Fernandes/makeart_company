045bf03cd244d9df8d525d775b3b55d83ca914d4
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-04-26 22:55:14 -0300

    iniciando estrutura do projeto

---
+++ makeart-server/src/main/java/com/makeart/makeart_server/MakeartServerApplication.java
@@ -0,0 +1,13 @@
+package com.makeart.makeart_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeartServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeartServerApplication.class, args);
+	}
+
+}
---
+++ makeart-server/src/test/java/com/makeart/makeart_server/MakeartServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeart.makeart_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeartServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
