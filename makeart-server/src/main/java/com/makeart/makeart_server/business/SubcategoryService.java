eef38340f9aa512d70bba7a98750ae9d8b94cafb
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-04-28 19:31:05 -0300

    implementar cadastro de subcategoria com validacoes de codigo e descricao

--- makeart-server/src/main/java/com/makeart/makeart_server/business/SubcategoryService.java
+++ makeart-server/src/main/java/com/makeart/makeart_server/business/SubcategoryService.java
@@ -1,9 +1,56 @@
 package com.makeart.makeart_server.business;
 
+import com.makeart.makeart_server.infrastructure.entity.Subcategory;
+import com.makeart.makeart_server.infrastructure.exceptions.ConflictException;
+import com.makeart.makeart_server.infrastructure.repository.SubcategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SubcategoryService {
+
+    private final SubcategoryRepository subcategoryRepository;
+
+    public Subcategory registerSubcategory(Subcategory subcategory) {
+        try {
+            codeExist(subcategory);
+            descriptionExist(subcategory);
+            return subcategoryRepository.save(subcategory);
+        } catch (ConflictException e) {
+            throw new ConflictException(e.getMessage());
+        }
+    }
+
+    public void codeExist(Subcategory subcategory) {
+        try {
+            boolean exist = codeAlreadyExists(subcategory);
+
+            if (exist) {
+                throw new ConflictException("Código para subcategoria já cadastrado " + subcategory.getCode());
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("Erro ao verificar código " + e.getMessage());
+        }
+    }
+
+    public void descriptionExist(Subcategory subcategory) {
+        try {
+            boolean exist = descriptionAlreadyExists(subcategory);
+
+            if (exist) {
+                throw new ConflictException("Subcategoria já cadastrada " + subcategory.getDescription());
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("Erro ao verificar descrição " + e.getMessage());
+        }
+    }
+
+    public boolean codeAlreadyExists(Subcategory subcategory) {
+        return subcategoryRepository.existsByCode(subcategory.getCode());
+    }
+
+    public boolean descriptionAlreadyExists(Subcategory subcategory) {
+        return subcategoryRepository.existsByDescription(subcategory.getDescription());
+    }
 }
