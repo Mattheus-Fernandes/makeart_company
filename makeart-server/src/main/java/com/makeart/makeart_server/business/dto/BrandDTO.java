20689f017cfae859a31da0c3ec33dd786fdfd1ed
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-05-10 13:45:05 -0300

    criado requisicoes para buscar marcas

---
+++ makeart-server/src/main/java/com/makeart/makeart_server/business/dto/BrandDTO.java
@@ -0,0 +1,13 @@
+package com.makeart.makeart_server.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BrandDTO {
+    private String code;
+    private String description;
+}
