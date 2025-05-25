f99a75c9c1008e7d0402686ea4fdc17aacad8363
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-05-24 21:52:44 -0300

    refatoracao no filtro de busca de categorias

--- makeart-server/src/main/java/com/makeart/makeart_server/infrastructure/repository/CategoryRepository.java
+++ makeart-server/src/main/java/com/makeart/makeart_server/infrastructure/repository/CategoryRepository.java
@@ -2,10 +2,14 @@ package com.makeart.makeart_server.infrastructure.repository;
 
 import com.makeart.makeart_server.infrastructure.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByCode(String code);
     boolean existsByDescription(String description);
     Optional<Category> findByCode(String code);
+    List<Category> findByCodeContainsIgnoreCase(String code);
+    List<Category> findByDescriptionContainsIgnoreCase(String description);
 }
